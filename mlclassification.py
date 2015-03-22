9de0f9e3291ffe9793e4bfd58558a9c4036b2c99
chihchunchen <chihchunchen>
2015-03-22 00:28:45 +0000

    boilerplate

---
+++ mlclassification.py
@@ -0,0 +1,15 @@
+import csv
+import numpy
+import scipy
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+from sklearn import neighbors
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+import knnplots
+
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn import cross_validation
+from sklearn.grid_search import GridSearchCV
